ca283e8109a5acf8c99309979936e6bfadc7f261
Yuriy Ivanchev <uivanchev@initium.ru>
2024-02-02 19:36:45 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	input := "Hello, OTUS!"
+	reverseInput := reverse.String(input)
+	fmt.Println(reverseInput)
 }
