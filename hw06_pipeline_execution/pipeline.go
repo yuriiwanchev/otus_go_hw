8ab82d00ed236bcf4d31a73f055f15229d403d0b
Yuriy Ivanchev <uivanchev@initium.ru>
2024-04-30 19:28:13 +0300

    ExecutePipeline func

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +13,56 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+
+	out := doStage(done, stages[0], in)
+
+	for i := 1; i < len(stages); i++ {
+		out = doStage(done, stages[i], out)
+	}
+
+	return out
+}
+
+func doStage(done In, stage Stage, in In) Out {
+	valueStream := make(Bi)
+	out := stage(in)
+
+	closeStream := make(chan bool)
+
+	stopWrite := false
+
+	mutex := sync.Mutex{}
+
+	go func() {
+		for s := range out {
+			mutex.Lock()
+			if !stopWrite {
+				valueStream <- s
+				mutex.Unlock()
+				continue
+			}
+			mutex.Unlock()
+			break
+		}
+
+		closeStream <- true
+	}()
+
+	go func() {
+		<-done
+		closeStream <- true
+	}()
+
+	go func() {
+		<-closeStream
+		mutex.Lock()
+		stopWrite = true
+		close(valueStream)
+		mutex.Unlock()
+	}()
+
+	return valueStream
 }
