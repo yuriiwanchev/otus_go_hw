f4e4307d7a71206b105a43b9765cedc0dd6f359d
Yuriy Ivanchev <uivanchev@initium.ru>
2024-05-09 00:13:14 +0300

    hw done

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,184 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"math"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	fromFilePath := "testdata/input.txt"
+
+	t.Run("copy all", func(t *testing.T) {
+		// tempFile, err := os.CreateTemp("", "out.txt")
+		// if err != nil {
+		// 	t.Fatalf("Failed to create temp file: %v", err)
+		// }
+
+		// Arrange
+		outFilePath := "testdata/out1.txt"
+		checkFilePath := fromFilePath
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, 0, 0)
+		defer os.Remove(outFilePath)
+		isSame, compareErr := deepCompare(checkFilePath, outFilePath)
+
+		// Assert
+		require.NoError(t, copyErr)
+		require.NoError(t, compareErr)
+		assert.True(t, isSame, "files should be the same")
+	})
+
+	t.Run("copy limit without offset", func(t *testing.T) {
+		// Arrange
+		outFilePath := "testdata/out2.txt"
+		checkFilePath := "testdata/out_offset0_limit1000.txt"
+		offset := int64(0)
+		limit := int64(1000)
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, offset, limit)
+		defer os.Remove(outFilePath)
+		isSame, compareErr := deepCompare(checkFilePath, outFilePath)
+		outFileNumberOfBytes, bytesErr := getFileNumberOfBytes(outFilePath)
+
+		// Assert
+		require.NoError(t, copyErr)
+		require.NoError(t, compareErr)
+		require.NoError(t, bytesErr)
+		assert.True(t, isSame, "files should be the same")
+		assert.Equal(t, outFileNumberOfBytes, limit, "coped number of bytes should be equal to limit")
+	})
+
+	t.Run("copy limit with offset", func(t *testing.T) {
+		// Arrange
+		outFilePath := "testdata/out3.txt"
+		checkFilePath := "testdata/out_offset100_limit1000.txt"
+		offset := int64(100)
+		limit := int64(1000)
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, offset, limit)
+		defer os.Remove(outFilePath)
+		isSame, compareErr := deepCompare(checkFilePath, outFilePath)
+		outFileNumberOfBytes, bytesErr := getFileNumberOfBytes(outFilePath)
+
+		// Assert
+		require.NoError(t, copyErr)
+		require.NoError(t, compareErr)
+		require.NoError(t, bytesErr)
+		assert.True(t, isSame, "files should be the same")
+		assert.Equal(t, outFileNumberOfBytes, limit, "coped number of bytes should be equal to limit")
+	})
+
+	t.Run("copy all with offset", func(t *testing.T) {
+		// Arrange
+		outFilePath := "testdata/out4.txt"
+		checkFilePath := "testdata/out_offset100_limit0.txt"
+		offset := int64(100)
+		limit := int64(0)
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, offset, limit)
+		defer os.Remove(outFilePath)
+		isSame, compareErr := deepCompare(checkFilePath, outFilePath)
+
+		// Assert
+		require.NoError(t, copyErr)
+		require.NoError(t, compareErr)
+		assert.True(t, isSame, "files should be the same")
+	})
+
+	t.Run("copy limit with offset if limit > file size", func(t *testing.T) {
+		// Arrange
+		outFilePath := "testdata/out5.txt"
+		checkFilePath := "testdata/out_offset6000_limit1000.txt"
+		offset := int64(6000)
+		limit := int64(1000)
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, offset, limit)
+		defer os.Remove(outFilePath)
+		isSame, compareErr := deepCompare(checkFilePath, outFilePath)
+		outFileNumberOfBytes, bytesErr := getFileNumberOfBytes(outFilePath)
+
+		// Assert
+		require.NoError(t, copyErr)
+		require.NoError(t, compareErr)
+		require.NoError(t, bytesErr)
+		assert.True(t, isSame, "files should be the same")
+		assert.Less(t, outFileNumberOfBytes, limit, "coped number of bytes should be less than limit")
+	})
+
+	t.Run("copy limit with offset > file size", func(t *testing.T) {
+		// Arrange
+		outFilePath := "testdata/out6.txt"
+		offset := int64(math.MaxInt64)
+		limit := int64(0)
+
+		// Act
+		copyErr := Copy(fromFilePath, outFilePath, offset, limit)
+		defer os.Remove(outFilePath)
+
+		// Assert
+		assert.Error(t, copyErr)
+		assert.ErrorIs(t, copyErr, ErrOffsetExceedsFileSize)
+	})
+}
+
+const chunkSize = 1024
+
+func deepCompare(file1, file2 string) (bool, error) {
+	f1, err := os.Open(file1)
+	if err != nil {
+		return false, err
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		return false, err
+	}
+	defer f2.Close()
+
+	for {
+		b1 := make([]byte, chunkSize)
+		_, err1 := f1.Read(b1)
+
+		b2 := make([]byte, chunkSize)
+		_, err2 := f2.Read(b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true, nil // Both files reached EOF, they are the same
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false, nil // One file reached EOF before the other, they are not the same
+			}
+
+			return false, err1 // Both files returned an error, they are not the same
+		}
+
+		if !bytes.Equal(b1, b2) {
+			return false, nil // Chunks are not equal, files are not the same
+		}
+	}
+}
+
+func getFileNumberOfBytes(filePath string) (int64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	info, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return info.Size(), nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,19 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/yuriiwanchev/otus_go_hw/hw07_file_copying
 
 go 1.19
+
+require (
+	github.com/stretchr/testify v1.9.0
+	github.com/vbauerster/mpb/v7 v7.5.3
+)
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220909162455-aba9fc2a8ff2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
