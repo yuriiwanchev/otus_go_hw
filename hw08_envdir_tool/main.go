953555d471e417532aad0342745551ac6c8ee451
yuriiwanchev <yura.iwanchev@yandex.ru>
2024-05-26 22:19:42 +0300

    main

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <env_dir> <command> [args...]\n", os.Args[0])
+		return
+	}
+
+	envDir := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	code := RunCmd(cmdArgs, env)
+	os.Exit(code)
 }
