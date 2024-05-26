7ef05d1cdaca70f79e158b4f6f98eb3db9c83b74
yuriiwanchev <yura.iwanchev@yandex.ru>
2024-05-26 22:19:30 +0300

    executor + tests

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for key, value := range env {
+		_, exists := os.LookupEnv(key)
+
+		if exists {
+			err := os.Unsetenv(key)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error unsetting environment variable: %v\n", err)
+			}
+		}
+
+		if value.NeedRemove {
+			continue
+		}
+
+		os.Setenv(key, value.Value)
+	}
+
+	command := cmd[0]
+	args := cmd[1:]
+
+	cmdExec := exec.Command(command, args...)
+	cmdExec.Stdin = os.Stdin
+	cmdExec.Stdout = os.Stdout
+	cmdExec.Stderr = os.Stderr
+
+	slog.Info("running command", "cmd", cmdExec.String())
+
+	err := cmdExec.Run()
+	if err != nil {
+		exitErr, ok := err.(*exec.ExitError)
+		if ok {
+			return exitErr.ExitCode()
+		} else {
+			fmt.Fprintf(os.Stderr, "Error running command: %v\n", err)
+		}
+	}
+
+	returnCode = cmdExec.ProcessState.ExitCode()
+	return returnCode
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,80 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	// Create a temporary script file for testing
+	script := `#!/bin/sh
+if [ "$1" = "success" ]; then
+  exit 0
+else
+  exit 1
+fi
+`
+	scriptFile := "/testscript.sh"
+	err := os.WriteFile(scriptFile, []byte(script), 0755)
+	if err != nil {
+		t.Fatalf("Failed to create test script: %v", err)
+	}
+
+	defer os.Remove(scriptFile)
+
+	tests := []struct {
+		name       string
+		cmd        []string
+		env        Environment
+		wantStatus int
+	}{
+		{
+			name:       "successful command",
+			cmd:        []string{scriptFile, "success"},
+			env:        Environment{},
+			wantStatus: 0,
+		},
+		{
+			name:       "failing command",
+			cmd:        []string{scriptFile, "fail"},
+			env:        Environment{},
+			wantStatus: 1,
+		},
+		{
+			name:       "environment variable set",
+			cmd:        []string{scriptFile, "success"},
+			env:        Environment{"TEST_ENV": {Value: "value", NeedRemove: false}},
+			wantStatus: 0,
+		},
+		{
+			name:       "environment variable removed",
+			cmd:        []string{scriptFile, "success"},
+			env:        Environment{"TO_DELETE_ENV": {Value: "", NeedRemove: true}},
+			wantStatus: 0,
+		},
+		{
+			name:       "environment variable set to delete and set again",
+			cmd:        []string{scriptFile, "success"},
+			env:        Environment{"SWAP_ENV": {Value: "other_value", NeedRemove: false}},
+			wantStatus: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv("TO_DELETE_ENV", "value")
+			os.Setenv("SWAP_ENV", "value")
+
+			gotStatus := RunCmd(tt.cmd, tt.env)
+			for key, value := range tt.env {
+				envValue, exists := os.LookupEnv(key)
+				assert.True(t, exists)
+				assert.Equal(t, value, envValue)
+			}
+
+			assert.Equal(t, tt.wantStatus, gotStatus)
+		})
+	}
 }
